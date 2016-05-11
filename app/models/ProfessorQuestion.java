dd2812b6216bd20d5a256d259ef6e4de06e4ca75
florenceC <Florence@172.16.234.73>
2016-05-11 14:06:12 +0200

    classe java créé pour professor

---
+++ app/models/ProfessorQuestion.java
@@ -0,0 +1,18 @@
+package models;
+
+import play.mvc.*;
+import views.html.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import play.Logger;
+import play.data.DynamicForm;
+import play.data.Form;
+import play.db.*;
+
+public class ProfessorQuestion {
+
+}
