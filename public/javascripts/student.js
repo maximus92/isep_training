f02815e465f873d90b68da24cb80e38581f06ca8
Mathieu Girardon <math.girardon@hotmail.fr>
2016-05-23 16:39:27 +0200

    enregistrement des paramètres qcm

--- public/javascripts/student.js
+++ public/javascripts/student.js
@@ -27,6 +27,8 @@ $("#back-to-settings-first-part").click( function(){
 });
 
 // Sélection du modules
+var id_module;
+var id_chapter;
 
 $("#module > li").click(function(){
 	$("#module > li").css({
@@ -38,16 +40,36 @@ $("#module > li").click(function(){
 		'color': 'white'
 	});
 	
+	$(".selected-module").removeClass("selected-module");
+	$(this).addClass("selected-module");
+	
 	// Récupération de l'id du module selectionné
-	var moduleId = $(this).attr("id").substring(10,11);
+	id_module = $(this).attr("id").substring(10,11);
 
 	// Cacher les Chapitres affichés
 	$(".chapters-list").hide();
 	
 	// Afficher les chapitres correspondants au module selectionné
-	$("#chapters-module-" + moduleId).show();
+	$("#chapters-module-" + id_module).show();
 	
 
+});
+
+// selection du chapitre
+
+$(".chapters-list > li").click(function(){
+	$(".chapters-list > li").css({
+		'background-color': 'white',
+		'color': '#333'
+	});
+	$(this).css({
+		'background-color': '#0D6186',
+		'color': 'white'
+	});
+	
+	// Récupération de l'id du chapitre selectionné
+	id_chapter = $(this).attr("id").substring(11,12);
+	
 });
 
 // Cacher les chapitres au chargement de la page
@@ -56,5 +78,26 @@ $(window).load(function(){
 	$(".chapters-list").hide();
 });
 	
+// Récupération des paramètres selectionnés
+
+var qcm_json_settings;
+
+$("#send-qcm-settings").click(function(){
+	var question_num = parseInt($("#number-of-questions").find(":selected").text());
+	var qcm_time = parseInt($("#qcm-time-hours").find(":selected").text())*60 + parseInt($("#qcm-time-minutes").find(":selected").text());
+	var question_level = parseInt($("#question-level").find(":selected").text());
+	
+	qcm_json_settings = {
+			"id_module": id_module,
+			"id_chapter": id_chapter,
+			"question_num": question_num,
+			"qcm_time": qcm_time,
+			"question_level": question_level
+	};
+	
+	alert(JSON.stringify(qcm_json_settings, null, '\t'));
+	
+});
+
 
 });
