ade706e2f589e0d2aae2bc1f15576620db540992
maximus92 <max.abd94@gmail.com>
2016-06-03 18:08:55 +0200

    Restructuration JS 2.0

--- public/javascripts/some-views.js
+++ public/javascripts/some-views.js
@@ -128,5 +128,128 @@
                     '</div></div>';
       return div;              
     }
+    
+    function questionUpdateDiv(data){
+      var option1 = "";
+      var option2 = "";
+      var option3 = "";
+      var option4 = "";
+      var option5 = "";
+      
+      switch (data[1][0].level){
+        case "0":
+          option1 = "selected";
+          break;
+        case "1":
+          option2 = "selected";
+          break;
+        case "2":
+          option3 = "selected";
+          break;
+      }
+      
+       var difficulty = '<div class="col-xs-3">'+
+                          '<select name="level">'+
+                               '<option value="0" '+option1+'>Facile</option>'+
+                               '<option value="1" '+option2+'>Moyen</option>'+
+                               '<option value="2" '+option3+'>Difficile</option>'+
+                          '</select>'+
+                        '</div>';	 
+      
+      
+      if(data[1][0].forexam == "0"){
+         option4 = "selected";
+      }else{
+         option5 = "selected";
+      }
+      
+      var forexam = '<div class="col-xs-2">'+
+                        '<SELECT name="forexam">'+
+                            '<OPTION value="0" '+option4+'>Entrainement des élèves</OPTION>'+
+                            '<OPTION value="1" '+option5+'>Examen</OPTION>'+
+                        '</SELECT>'+
+                    '</div>';
+                    	 
+     var formulaire1 = '<form method="post" class="form_update_question" action="@routes.ProfessorController.updateA()">'+
+                            '<div class="row">'+
+                              '<div class="col-xs-2">'+
+                                '<label>Difficulté : </label>'+
+                              '</div>'+difficulty+
+                              '<div class="col-xs-3">'+
+                                  '<label>Question pour : </label>'+
+                              '</div>'+forexam+
+                            '</div>'+
+                            '</br>'+
+                            '<div class="row">'+
+                              '<div class="col-xs-2 ">'+
+                                  '<label for="module">Module: </label>'+
+                              '</div>'+
+                              '<div class="col-xs-3">'+
+                                '<select id="test_module" name="test_module" size="1"></select>'+
+                              '</div>'+
+                              '<div class="col-xs-2">'+
+                                '<label for="chapter">Chapitre: </label>'+
+                              '</div>'+
+                              '<div class="col-xs-3">'+
+                                '<select name="test_chapter" size="1">'+
+                                  '<option value="">Aucun</option>'+
+                                '</select>'+
+                              '</div>'+
+                           '</div>'+
+                           '</br>'+
+                           '<div class="form-group">'+
+                             '<label for="question">Question</label> <input type="text"'+
+                               'class="form-control" id="question" name="question" value="'+data[1][0].question+'">'+
+                             '<div class="pull-right">'+
+                               '<button type="button" class="btn btn-success btn-sm">&</button>'+
+                             '</div>'+
+                           '</div>'+
+                           '</br> </br>'+
+                           '<div class="row">'+
+                             '<div class="col-xs-3 pull-right">'+
+                               '<label> Cocher les bonnes </br>réponses'+
+                               '</label>'+
+                             '</div>'+
+                           '</div>'+
+                           '</br>';
+
+         for(var i=0; i< data[0].length; i++){
+              if(data[0][i].istrue == "0"){
+                var checkA = "";
+              }else{
+                var checkA = "checked";
+              }
+             var isTrue =  '<div class="col-xs-1">'+
+                             '<label> <input type="checkbox" value="'+data[0][i].istrue+'" name="goodA'+data[0][i].id_answer+'" '+checkA+'>'+
+                             '</label>'+
+                           '</div>';
+           
+                formulaire1 += '<div id="addQuestionA">'+
+                             '<input type="hidden" value="1" name="reponse_counter"'+
+                               'id="reponse_counter" />'+
+                             '<div id="remove0">'+
+                               '<label for="reponse">Réponse</label>'+
+                               '<div class="row">'+
+                                 '<div class="col-xs-10">'+
+                                   '<input type="text" class="form-control answer-label" id="reponse'+data[0][i].id_answer+'"'+
+                                     'value="'+data[0][i].answer+'">'+
+                                 '</div>'+isTrue+
+                                 '<div class="col-xs-1">'+
+                                   '<i class="fa fa-times delete_answer" id="delete_answer0"></i>'+
+                                 '</div>'+
+                               '</div>'+
+                             '</div>'+
+                           '</div>';
+        }
+         
+         formulaire1 += '</br>'+
+                           '<button type="button" class="btn btn-primary pull-left"'+
+                           'id="addAnswer">Ajouter une réponse</button>'+
+                         '</br> </br> </br> <label for="correction">Correction détaillée</label>'+
+                         '<textarea rows="4" cols="50" class="form-control" name="correction">'+data[1][0].correction+'</textarea>'+
+                         '</br>'+
+                       '</form>';
+     return formulaire1;  
+    }
 
   
